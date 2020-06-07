876ef2e931d8f3cd4237efa662163613f02e642f
Silvoney Machado <silvoneymachado@gmail.com>
2020-06-07 13:26:32 -0300

    Add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () =>{
+  return (
+    <View style={styles.container}>
+      <Text>Helo world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+export default App;
