009ddfa729daf84632e40822fcd3fd8f3f2c91e9
Silvoney Machado <silvoneymachado@gmail.com>
2020-06-11 11:15:07 -0300

    Add swagger docs to API

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Next Level Week 001
-Next Level Week - Rocketseat - Node, ReactJs, React Native com expo
+## Next Level Week - Rocketseat - Node, ReactJs, React Native with expo
+
+- This week, we created a full stack project that aims to mediate waste collection between entities/local businesses and customers/population.
+   - We've used:
+   - NodeJS in back-end
+   - ReactJS in front-ent
+   - ReactNative with expo in app mobile 
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,17 @@ import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 import { errors } from "celebrate";
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 
 const app = express();
+
+
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(errors());
 
